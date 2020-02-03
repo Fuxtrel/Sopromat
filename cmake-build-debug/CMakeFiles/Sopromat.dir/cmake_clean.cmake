edfc17b075796159c7d11d3cfdafc0d68da3b763
Fuxtrel <a99som@yandex.ru>
2020-02-03 17:29:09 +0300

    Пустейший проект, начало!

---
+++ cmake-build-debug/CMakeFiles/Sopromat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sopromat.dir/main.cpp.obj"
+  "Sopromat.exe"
+  "Sopromat.exe.manifest"
+  "Sopromat.pdb"
+  "libSopromat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sopromat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
